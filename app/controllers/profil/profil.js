9eec14b3357d50b42eebc0fc9d3bb0f8f080bf2e
Thomas LEMAITRE <thomas@squirrel.fr>
2020-04-01 18:23:40 +0400

    add new feat

--- app/controllers/profil/profil.js
+++ app/controllers/profil/profil.js
@@ -7,19 +7,19 @@
 import { User } from "/classes/user";
 
 Alloy.Globals.currentUser = Ti.App.Properties.getObject("user", null)
-  ? new User(Ti.App.Properties.getObject("user"))
-  : null;
+	? new User(Ti.App.Properties.getObject("user"))
+	: null;
 /**
  * @method Controller
  * Display profil view
  * @param  {Arguments} args Arguments passed to the controller
  */
 (function constructor(args) {
-  $.paging.setScrollableView($.scrollableView);
-  if (Alloy.Globals.currentUser) {
-    $.top.photo.image = Alloy.Globals.currentUser.getAvatar();
-    $.top.name.text = Alloy.Globals.currentUser.getFullName();
-  }
+	$.paging.setScrollableView($.scrollableView);
+	if (Alloy.Globals.currentUser) {
+		$.top.photo.image = Alloy.Globals.currentUser.avatar;
+		$.top.name.text = Alloy.Globals.currentUser.fullname;
+	}
 })($.args);
 
 /**
@@ -29,12 +29,12 @@ Alloy.Globals.currentUser = Ti.App.Properties.getObject("user", null)
  * @return {type}   description
  */
 function openDialogCamera(e) {
-  require("/media").openDialogCamera(function(photo, ext) {
-    $.top.photo.image = photo;
-    var name = require("/media").saveFile({
-      blob: photo,
-      ext: ext
-    });
-    require("dao/variable").set("photo", name);
-  });
+	require("/media").openDialogCamera(function(photo, ext) {
+		$.top.photo.image = photo;
+		var name = require("/media").saveFile({
+			blob: photo,
+			ext: ext,
+		});
+		require("dao/variable").set("photo", name);
+	});
 }
